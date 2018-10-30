0818ce092d98ac80ff4f9021a63a8e84d8b27973
Lok Kwan <lokkwanfoo@hotmail.com>
2018-10-30 17:34:55 +0100

    no message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,29 @@ const template = require('./app.component.html');
     selector: 'app-home',
     template
 })
+
 export default class AppComponent {
     welcomeMessage = 'Welcome';
+
+    createContentControl() {
+        Word.run(function (context) {
+    
+            const serviceNameRange = context.document.getSelection();
+            const serviceNameContentControl = serviceNameRange.insertContentControl();
+            serviceNameContentControl.title = "Service Name";
+            serviceNameContentControl.tag = "serviceName";
+            serviceNameContentControl.appearance = "Tags";
+            serviceNameContentControl.color = "blue";
+                
+            return context.sync();
+        })
+        .catch(function (error) {
+            console.log("Error: " + error);
+            if (error instanceof OfficeExtension.Error) {
+                console.log("Debug info: " + JSON.stringify(error.debugInfo));
+            }
+        });
+    }
 
     async run() {
         return Word.run(async context => {
