15c421ececab765db714fbb5c2285752c4c34b93
Lok Kwan Foo <lokkwanfoo@hotmail.com>
2018-11-05 01:39:26 +0100

    no message

---
+++ src/app/app.routing.ts
@@ -0,0 +1,32 @@
+import { NgModule }             from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import AppComponent from './app.component';
+import { LetterComponent } from '../letter/letter.component';
+
+// const routes: Routes = [
+//     {
+//         path: '',
+//         component: AppComponent
+//     },
+//     { 
+//         path: 'users', 
+//         component: UsersComponent 
+//     }
+//   ];
+
+  @NgModule({
+    imports: [ RouterModule.forRoot([
+        {
+            path: 'home',
+            component: AppComponent
+        },
+        {
+          path: 'letter',
+          component: LetterComponent
+      }
+      ]) ],
+    exports: [ RouterModule ]
+  })
+
+export class AppRoutingModule {}
---
+++ src/letter/letter.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'app-letter',
+  templateUrl: './letter.component.html',
+})
+
+export class LetterComponent {
+
+}
